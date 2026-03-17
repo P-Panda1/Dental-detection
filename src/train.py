e6e5a641435cdde87f73f8cd7d8388b23d76087e
Peeyush Patel <patelpeeyush2000@gmail.com>
2026-03-17 14:58:49 -0700

    train updated

--- src/train.py
+++ src/train.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 
 # Import custom modules
 from config import config
-from model import DentalMetricDGCNN, DentalPointTransformer
+from model import DentalMetricDGCNN, DentalBoundaryDGCNN
 from data_loader import get_dental_loaders
 
 import torch
@@ -139,17 +139,17 @@ def train():
     train_loader, val_loader, _ = get_dental_loaders(
         "../data", batch_size=config.BATCH_SIZE)
 
-    model = DentalMetricDGCNN(
-        k=config.K_NEIGHBORS,
-        num_classes=config.NUM_CLASSES,
-        embed_dim=config.EMBEDDING_DIM
-    ).to(config.DEVICE)
-
-    # model = DentalPointTransformer(
+    # model = DentalMetricDGCNN(
     #     k=config.K_NEIGHBORS,
     #     num_classes=config.NUM_CLASSES,
     #     embed_dim=config.EMBEDDING_DIM
     # ).to(config.DEVICE)
+
+    model = DentalBoundaryDGCNN(
+        k=config.K_NEIGHBORS,
+        num_classes=config.NUM_CLASSES,
+        embed_dim=config.EMBEDDING_DIM
+    ).to(config.DEVICE)
 
     # model.compile()  # Optional: Use PyTorch 2.0 compilation for potential speedup
 
